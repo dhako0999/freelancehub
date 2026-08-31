501beb5591a3c583ae8bce8a65fbbcf711e686ef
Dhanu Konara <Dkonara@gmail.com>
2026-08-31 14:05:42 -0400

    Fix local Docker config and refine registration layout

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,13 @@
-
 unless ENV["SECRET_KEY_BASE_DUMMY"]
+  redis_url = ENV.fetch("REDIS_URL") do
+    Rails.env.production? ? raise(KeyError, "REDIS_URL is required in production") : "redis://redis:6379/0"
+  end
+
   Sidekiq.configure_server do |config|
-    config.redis = { url: ENV.fetch("REDIS_URL") }
+    config.redis = { url: redis_url }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: ENV.fetch("REDIS_URL") }
+    config.redis = { url: redis_url }
   end
 end
